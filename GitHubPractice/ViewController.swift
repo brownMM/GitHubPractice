4b3bef9b4341c59cd603aa65f40374ebb84baea5
Bob Brown <bob.brown@d214.org>
2021-07-22 11:29:30 -0500

    Initial SetUp

--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
