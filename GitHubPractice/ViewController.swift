aa45847ca98fccc7adfb5584b147c1a6a69c45cf
Bob Brown <bob.brown@d214.org>
2021-07-22 11:27:28 -0500

    Initial Commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Robert D. Brown on 7/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
